ac360add0ef5a094f3f9b0eaa182e754bab09cc1
Travis Gall <travis.gall@gmail.com>
2020-05-08 04:01:04 +0000

    DMNC-63: update visual menu with new query

--- http/assets/php/menu.php
+++ http/assets/php/menu.php
@@ -13,14 +13,6 @@ function pages($conn) { // {{{2
   return myquery($query);
 }
 
-function category_items($conn, $category) { // {{{2
-    $queryStr = '
-SELECT * FROM items, categories_items
-WHERE categories_items.items_id = items.id
-AND categories_items.id = "' . $category['id'] . '"';
-  return $items;
-}
-
 function categories($conn, $page) { // {{{2
   $page_query = myquery($conn, '
 SELECT * FROM pages WHERE name = "' . $page . '"');
@@ -46,7 +38,7 @@ AND pages_categories.pages_id = "' . $page['id'] . '"';
   return $categories;
 }
 
-function items($conn, $categories_id) { // {{{2
+function categories_items($conn, $categories_id) { // {{{2
   $items[] = array();
   $queryStr = '
 SELECT * FROM items, categories_items
@@ -60,69 +52,7 @@ AND categories_items.items_id = items.id';
   return $items;
 }
 
-function menu($conn, $query, $category, $description, $col, $allowHeader=true) { // {{{2
-  $results = $conn->query($query);
-  if ($results->num_rows > 0) {
-    // element($col, "h2", array(), $category);
-
-    $row = element($col, 'div', array('class'=>'row'));
-    element($row, 'div', array('class'=>'col-1'));
-    $col = element($row, 'div', array('class'=>'col-11'));
-    $pricingTable = element($col, "div", array("class"=>"pricingTable"));
-    element($pricingTable, "h2", array('id'=>$category, "class"=>"pricingTable-title"), $category);
-    element($pricingTable, "h3", array("class"=>"pricingTable-subtitle"), $description);
-    $row = element($pricingTable, "div", array("class"=>"row"));
-
-    // Body
-    $i = 0;
-    while($result = $results->fetch_assoc()) {
-      $query = "SELECT * FROM menu_items_toppings JOIN menu_toppings ON menu_items_toppings.menu_topping_id WHERE menu_item_id = " . $result["id"];
-      $results_toppings = $conn->query($query);
-      $toppings = array();
-      while($topping = $results_toppings->fetch_assoc())
-        $toppings[] = $topping["name"];
-      if ($i % 2 == 0) {
-        $row = element($pricingTable, "div", array("class"=>"row"));
-        element($row, "div", array("class"=>"col-1"));
-      }
-      menuItem($pricingTable, $row, $result['name'], number_format($result['price'], 2), $toppings);
-      $i++;
-    }
-  }
-}
-
-function menuTR($tbody, $type, $name, $description, $toppings, $price) { // {{{2
-  $tr = element($tbody, 'tr');
-  $td = element($tr, $type);
-  $p = element($td, "p", array("style"=>"margin-bottom:5px;"), $name);
-  $td = element($tr, $type);
-  $p = element($td, "p", array("style"=>"margin-bottom:5px;"), $description);
-  $td = element($tr, $type);
-  $p = element($td, "p", array("style"=>"margin-bottom:5px;"), $toppings);
-  $td = element($tr, $type);
-  $p = element($td, "p", array("style"=>"margin-bottom:5px;"), $price);
-}
-
-function menus($conn, $panel, $menu) { // {{{2
-  // $query = "SELECT * FROM menu_categories";
-  $query = 'SELECT * FROM `menu_categories` WHERE name = "' . $menu . '"';
-  $categories = $conn->query($query);
-
-  if ($categories->num_rows > 0) {
-    // output data of each row
-    $row = element($panel, "div", array("class"=>"row"));
-    $colFresh = element($row, "div", array("class"=>"col-12 text-center"));
-
-    while($category = $categories->fetch_assoc()) {
-      // Fresh
-      $query = "SELECT * FROM menu_items WHERE category_id=" . $category["id"];
-      $results = $conn->query($query);
-      menu($conn, $query, $category["name"], $category["description"], $colFresh);
-    }
-  }
-}
-
-function menuItem($menu, $element, $name, $price, $toppings) { // {{{2
+function menuItem($element, $name, $price, $toppings) { // {{{2
   $rowCol = element($element, "div", array("class"=>"col-md-5 text-center"));
   $row = element($rowCol, "div", array("class"=>"row pricingTable-firstTable"));
 
--- http/instoremenu.php
+++ http/instoremenu.php
@@ -12,11 +12,7 @@ if($ORDERFORM)
 else {
   $row = element($panel, "div", array("class"=>"row"));
   $col = element($row, "div", array("class"=>"col-12 text-center"));
-  element($col, "h1", array(), "In Store Menu");
-  menus($conn, $panel, 'Pizza');
-  menus($conn, $panel, 'Drinks');
-  menus($conn, $panel, 'Desserts');
-
+  
   $name = array();
   $results = categories($conn, 'In Store Menu');
   foreach($results as $result)
@@ -24,20 +20,27 @@ else {
 
   foreach($categories as $category) {
     element($col, 'h1', array(), $category['name']);
-    $items = items($conn, $category['categories_id']);
+    $items = categories_items($conn, $category['categories_id']);
     foreach($items as $item) {
-      element($col, 'p', array(), $item['name']);
-      element($col, 'p', array(), $item['shortdesc']);
-      element($col, 'p', array(), $item['description']);
-      element($col, 'p', array(), $item['imagename']);
-      element($col, 'p', array(), $item['notes']);
-      element($col, 'p', array(), $item['instock']); // Bool
-      element($col, 'p', array(), $item['acrive']); // Bool
+      if ($item['name'] != ''){
+        // element($col, 'p', array(), $item['name']);
+      // element($col, 'p', array(), $item['shortdesc']);
+      // element($col, 'p', array(), $item['description']);
+      // element($col, 'p', array(), $item['imagename']);
+      // element($col, 'p', array(), $item['notes']);
+      // element($col, 'p', array(), $item['instock']); // Bool
+      // element($col, 'p', array(), $item['acrive']); // Bool
+      if ($i % 2 == 0) {
+        $row = element($col, "div", array("class"=>"row"));
+        element($row, "div", array("class"=>"col-1"));
+      }
+      // menuItem($row, $item['name'], $item['id'], array($item['description']));
+      menuItem($row, $item['name'], $item['id'], array($item['shortdesc']));
+      $i++;
+      }
     }
   }
-
   navMenu($panel, $categories, $classNavMenu,  $styleNavMenu);
-
 }
 
 // Footer {{{1
