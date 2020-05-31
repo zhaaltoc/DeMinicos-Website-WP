7c14752a192fa331a4dcfc806caa306abdbc414e
Travis Gall <travis.gall@gmail.com>
2020-05-31 04:24:59 +0000

    DMNC-74: home icon background added to img and resize removed from css

--- http/assets/php/overview.php
+++ http/assets/php/overview.php
@@ -3,50 +3,37 @@
 // Authors: Travis Gall
 // Description: Main index
 
-//$imgLogo = $img . "/logo.png";
-$imgLogo = $img . "/DeMinicos_logo_color_reverse_spot-2.png";
+$imgLogo = $img . "/logo.png";
+// $imgLogo = $img . "/DeMinicos_logo_color_reverse_spot-2.png";
 $imgLogoNoStamp = $img . "/logonostamp.jpg";
 // $imgLogoNoStamp = $img . "/logostampcolor_reverse.jpg";
 
-// About Us {{{1
-$row = element($panel, "div", array("class"=>"row"));
+// Overview {{{1
+// Logo {{{2
+$row = element($panel, "div", array("class"=>"row", 'style'=>'padding-bottom:50px;'));
 $col = element($row, "div", array("class"=>"col-12 text-center"));
-// $div = element($col, "div", array("style"=>"padding-top:65px;"));
+$style .= 'height:6.5em;';
+$style .= 'width:6.5em;';
+$style .= 'max-width:100;';
+$hrefDeminicos = "https://test.deminicos.ca";
+$a = element ($col, "a", array("class"=>"socialIcon", "style"=>"color:#3b5998", 'href'=>'.'));
+element($a, "img", array("src"=>$imgLogo, 'style'=>$style));
 
-// Logo
- $style .= 'max-width:100%;';
- $hrefDeminicos = "https://test.deminicos.ca";
-  $a = element ($col, "a", array("class"=>"socialIcon", "style"=>"color:#3b5998", "href"=>$hrefDeminicos));
-  $span = element($a, "span", array("class"=>"logodot"));
-  //element($span, "img", array("src"=>$iconBestCalgary, 'style'=>$style));
-element($span, "img", array("src"=>$imgLogo, 'style'=>$style));
-
-$row = element($panel, "div", array("class"=>"text-center row"));
-$col = element($row, "div", array("class"=>"col-md-12", "style"=>"padding-bottom: 50px;"));
-
-// Description {{{2
+// Overview {{{2
+// About Us {{{3
 $row = element($panel, "div", array("class"=>"row"));
 $col = element($row, "div", array("class"=>"col-md-4", "style"=>"padding-top: 15px;"));
 element($col, "p", array("class"=>"text-center", "style"=>$styleFontP), "De Minico’s offers delicious pizza and paninis. Delight in authentic Italian flavours and try the Margherita or Artista pizza. Add a little kick to your day and spice things up with the Volta panini.");
 element($col, "p", array("class"=>"text-center", "style"=>$styleFontP), "Order De Minico’s");
 
-// TEST VUE {{{2
-// element($col, "div", array("id"=>"app"), "{{ message }}");
-
-// $dev = element($col, "div", array("id"=>"app-2"));
-// element($dev, "span", array("v-bind:title"=>"message"), "Mouse Over Me!");
-
-// $dev = element($col, "div", array("id"=>"app-3"));
-// element($dev, "span", array("v-if"=>"seen"), "Now you see me!");
-
-// Phone {{{2
+// Phone {{{3
 phone($col, "p", $phoneNumber, $classPhone, $stylePhoneP);
 
-// Social Media {{{2
+// Social Media {{{3
 element($col, "p", array("class"=>"text-center", "style"=>$styleFontP), "Learn more about us by following us on social media.");
 socialLinks($col);
 
-// Navigation {{{2
+// Navigation {{{3
 $col = element($row, "div", array("class"=>"col-md-4", "style"=>"padding-top:30px; text-align:center;"));
 if(!$ORDERFORM) {
   $results = pages($conn);
@@ -59,14 +46,13 @@ if(!$ORDERFORM) {
   navLink2($navLinks, "orderform", "Order Form", "/", $styleNavLinks);
 }
 
-// Hours {{{2
+// Hours {{{3
 $col = element($row, "div", array("class"=>"text-center col-md-4"));
 $row = element($col, "div", array("class"=>"row"));
 
 $col = element($row, "div", array("class"=>"col-12"));
 element($col, "h2", array("style"=>"padding-top:15px;"), "Hours");
 
-// $col = element($row, "div", array("class"=>"col-2"));
 $col = element($row, "div", array("class"=>"col-12", 'style'=>'text-align:right;'));
 $table = element($col, "table", array('style'=>'margin-left:-10px;width:100%;'));
 $tbody = element($table, "tbody");
@@ -77,4 +63,14 @@ hourRow($tbody, "Thursday", "11 A.M.", "6 P.M.", $styleDays, $styleHours);
 hourRow($tbody, "Friday", "11 A.M.", "8 P.M.", $styleDays, $styleHours);
 hourRow($tbody, "Saturday", "Closed", "", $styleDays, $styleHours);
 hourRow($tbody, "Sunday", "Closed", "", $styleDays, $styleHours);
+
+// TEST VUE {{{2
+// element($col, "div", array("id"=>"app"), "{{ message }}");
+
+// $dev = element($col, "div", array("id"=>"app-2"));
+// element($dev, "span", array("v-bind:title"=>"message"), "Mouse Over Me!");
+
+// $dev = element($col, "div", array("id"=>"app-3"));
+// element($dev, "span", array("v-if"=>"seen"), "Now you see me!");
+
 ?>
